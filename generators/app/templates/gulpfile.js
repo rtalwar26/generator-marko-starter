8e55fd2c6dbcc2b733fef788e635c7e9969263db
Rajat Talwar <cuterajat26@gmail.com>
2018-12-23 16:46:31 +0530

    add mobile support

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,49 +1,94 @@
 const gulp = require('gulp')
-
+const zipdir = require('zip-dir');
 const directoryName = 'docs';
-
+var outputDirectory = `./${directoryName}`
+var replace = require('gulp-replace');
+var rename = require("gulp-rename");
 const fs = require('fs')
+const packageConfig = JSON.parse(fs.readFileSync("./package.json", "utf8"));
 
 var exec = require('child_process').exec;
 
 function copy() {
 
-    return gulp.src(['./dist/**/*'])
-        .pipe(gulp.dest(`./${directoryName}`))
+  return gulp.src(['./dist/**/*', './package.json'])
+    .pipe(gulp.dest(`./${directoryName}`))
 
 }
 
+function makeAndroidReady() {
+  return gulp.src([`./dist/mobile/index.html`])
+    .pipe(replace(`${packageConfig.baseurl || ""}/static`, 'static'))
+    .pipe(rename("main.html"))
+    .pipe(gulp.dest(`./${directoryName}`));
+}
+
+function zipDistribution() {
+  return new Promise((resolve, reject) => {
+    zipdir(outputDirectory, {
+      saveTo: outputDirectory + "/dist.zip"
+    }, function (err, buffer) {
+      // `buffer` is the buffer of the zipped file
+      // And the buffer was saved to `~/myzip.zip`
+      if (err) {
+        console.log('unable to create zip', err)
+        reject(err);
+      } else {
+        console.log('zipping done')
+        resolve();
+      }
+    });
+  })
+
+}
 var del = require('del');
 
-function clean() {
+function cleanDist() {
   return del([
-    'docs/**/*',
-    'dist/**/*',    
+    './dist/**/*',
+    // here we use a globbing pattern to match everything inside the `mobile` folder
     '.cache/**/*'
   ]);
 }
+
+function cleanDocs() {
+  return del([
+    './docs/**/*',
+
+  ]);
+}
+
 exports.copy = copy;
 
 
 
 
-var build = gulp.series(clean,buildShell , copy);
+var build = gulp.series(cleanDist, cleanDocs, buildShellAndroid, makeAndroidReady, cleanDist, buildShell, copy);
+var buildMobile = gulp.series(cleanDist, cleanDocs, buildShellAndroid, makeAndroidReady, copy, cleanDist, buildShell, copy, zipDistribution);
+
+function buildShellAndroid(done) {
+  let isWindows = process.platform === "win32";
+  let cmd = isWindows ? `set ANDROID_APP=true && set LASSO_TIMEOUT=60000 && set NODE_ENV=production && marko-starter build` : `ANDROID_APP=true LASSO_TIMEOUT=60000 NODE_ENV=production marko-starter build`
+
+  exec(cmd, function (err, stdout, stderr) {
+    console.log(stdout);
+    err && console.log(err);
+    done();
+  });
+}
 
 function buildShell(done) {
-    let isWindows = process.platform === "win32";
-    let cmd = isWindows ? `set GITHUB_PUBLISH=true && set LASSO_TIMEOUT=60000 && set NODE_ENV=production && marko-starter build` :`GITHUB_PUBLISH=true LASSO_TIMEOUT=60000 NODE_ENV=production marko-starter build`
-    
-    exec(cmd, function (err, stdout, stderr) {
-        console.log(stdout);
-        err && console.log(err);
-        done();
-    });
+  let isWindows = process.platform === "win32";
+  let cmd = isWindows ? `set GITHUB_PUBLISH=true && set LASSO_TIMEOUT=60000 && set NODE_ENV=production && marko-starter build` : `GITHUB_PUBLISH=true LASSO_TIMEOUT=60000 NODE_ENV=production marko-starter build`
 
-
-
+  exec(cmd, function (err, stdout, stderr) {
+    console.log(stdout);
+    err && console.log(err);
+    done();
+  });
 }
 
 gulp.task('build', build);
 gulp.task('default', build);
-
+gulp.task('mobile', buildMobile);
 // gulp.task('watch', );
--- generators/app/templates/project.js
+++ generators/app/templates/project.js
@@ -1,21 +1,34 @@
 const fs = require("fs");
-const packageConfig = JSON.parse(fs.readFileSync("./package.json","utf8"));
+const packageConfig = JSON.parse(fs.readFileSync("./package.json", "utf8"));
 const isProduction = process.env.NODE_ENV && (process.env.NODE_ENV.trim() === 'production');
 const isGithubPublish = process.env.GITHUB_PUBLISH && (process.env.GITHUB_PUBLISH.trim() === 'true');
+const isAndroidPublish = process.env.ANDROID_APP && (process.env.ANDROID_APP.trim() === 'true');
 const project_name = "marko-starter"
 
-module.exports = require("marko-starter").projectConfig(isGithubPublish ? {
+module.exports = require("marko-starter").projectConfig({
   name: project_name, // Optional, but added here for demo purposes
   lassoConfig: {
-    outputDir: "dist/static",
+    require: {
+      transforms: [{
+        transform: 'lasso-babel-transform',
+        config: {
+          extensions: ['.js', '.es6'] // Enabled file extensions. Default: ['.js', '.es6']
+        }
+      }]
+    },
+    outputDir: isGithubPublish || isAndroidPublish ? `dist/static` : '.cache/static',
     bundlingEnabled: isProduction,
+    "bundles": [{
+      "name": "framework7",
+      "dependencies": [{
+        "path": "framework7/js/framework7.js"
+      }]
+    }],
     fingerprintsEnabled: isProduction,
-    urlPrefix:`${packageConfig.baseurl || ""}/static`,    
+    urlPrefix: isGithubPublish ? `${packageConfig.baseurl || ""}/static` : (isAndroidPublish ? 'static' : '/static'),
     minifyJS: false,
     plugins: [
-        'lasso-marko'
+      'lasso-marko'
     ]
-}
-} : {
-  name: project_name, // Optional, but added here for demo purposes
+  }
 });
