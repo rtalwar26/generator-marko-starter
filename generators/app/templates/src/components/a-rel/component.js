801f31e740af9bcb8ee221b612403e29cdd97258
Rajat Talwar <cuterajat26@gmail.com>
2019-01-23 15:07:26 +0530

    fix a-rel component for cordova build

--- generators/app/templates/src/components/a-rel/component.js
+++ generators/app/templates/src/components/a-rel/component.js
@@ -1,13 +1,16 @@
 const isGithubPublish = process.env.GITHUB_PUBLISH && (process.env.GITHUB_PUBLISH.trim() === 'true');
+const isCordovaPublish = process.env.CORDOVA_BUILD && (process.env.CORDOVA_BUILD.trim() === 'true');
+
 module.exports = class {
-    onInput(input,out) {
-        
-        
-        let isRelative = input.href && (input.href.charAt(0) === "/");
-        const packageConfig = JSON.parse(require("fs").readFileSync("./package.json","utf8"));
-        input.href = isGithubPublish && isRelative? `${packageConfig.baseurl}${input.href}` : input.href;        
-        input.class = input.class || "";
-       
-    }
-    
+  onInput(input, out) {
+
+
+    let isRelative = input.href && (input.href.charAt(0) === "/");
+    const packageConfig = JSON.parse(require("fs").readFileSync("./package.json", "utf8"));
+    input.href = isGithubPublish && isRelative ? `${packageConfig.baseurl}${input.href}` : input.href;
+    input.href = isCordovaPublish ? `${input.href}.html` : input.href;
+    input.class = input.class || "";
+
+  }
+
 }
